769330c422a7fdb28d597fa94e934811cd196176
amanrawt03 <amanrawat3021@gmail.com>
2024-09-14 23:45:37 +0530

    added eigen vector calculation in eigen.cpp

--- eigen.cpp
+++ eigen.cpp
@@ -7,13 +7,13 @@
 
  */
 
-#include<vector>
+#include <vector>
 #include "matrix.cpp"
 #include "vector.cpp"
 #include <iostream>
 #include "orthogonalization.cpp"
 
-#pragma once 
+#pragma once
 
 using namespace std;
 
@@ -27,24 +27,26 @@ using namespace std;
  * @return A new matrix where each element is rounded to the specified number of decimal places.
  */
 
-vector<vector<float>> rounder(vector<vector<float>> M) {
+vector<vector<float>> rounder(vector<vector<float>> M)
+{
 
 	vector<vector<float>> out;
 	const int decimal_place = 5;
 	auto decimal_values = pow(10, decimal_place);
 
-	for (int i = 0; i < M.size(); i++) {
+	for (int i = 0; i < M.size(); i++)
+	{
 		vector<float> row;
-		for (int j = 0; j < M[0].size(); j++) {
-			row.push_back(round(M[i][j]*decimal_values)/decimal_values);
+		for (int j = 0; j < M[0].size(); j++)
+		{
+			row.push_back(round(M[i][j] * decimal_values) / decimal_values);
 		}
 
 		out.push_back(row);
 	}
 
-	return(out);
+	return (out);
 }
-
 /**
  * @brief Calculates the R matrix in the QR decomposition of a given matrix.
  *
@@ -58,16 +60,73 @@ vector<vector<float>> rounder(vector<vector<float>> M) {
  * @return A matrix representing the R matrix in the QR decomposition, with each element rounded to a specified number of decimal places.
  */
 
-vector<vector<float>> R_Matrix_calc(matrix* M) {
+vector<vector<float>> R_Matrix_calc(matrix *M)
+{
 
 	auto ortho_mat = orthogonalization(M);
-	matrix* O = new matrix(ortho_mat);
+	matrix *O = new matrix(ortho_mat);
 	auto trans_mat = transpose(O);
-	matrix* T = new matrix(trans_mat);
-	auto prod =  matrix_product(T, M);
+	matrix *T = new matrix(trans_mat);
+	auto prod = matrix_product(T, M);
 	delete O;
 	delete T;
-	return(rounder(prod));
+	return (rounder(prod));
+}
+
+/**
+ * @brief Calculates whether a given vector is an eigenvector for a given matrix.
+ *
+ * This function performs matrix-vector multiplication of the input matrix `M` and vector `V`,
+ * and then checks if the result is a scalar multiple of the original vector `V`.
+ * If the result is a scalar multiple, the function returns the original vector, indicating it is an eigenvector.
+ * Otherwise, the function returns a null vector (a vector of zeroes), indicating that `V` is not an eigenvector of `M`.
+ *
+ * The function uses a tolerance to account for floating-point precision errors while comparing the ratios
+ * of the resulting vector and the original vector.
+ *
+ * @param M A reference to a 2D vector (matrix) for which the eigenvector check will be performed.
+ * @param V A reference to a 1D vector that is tested to see if it is an eigenvector of matrix `M`.
+ * @return A vector containing the original vector `V` if it is an eigenvector, or a null vector (all zeroes) if not.
+ */
+
+vector<float> eigenVectorCalc(const vector<vector<float>> &M, const vector<float> &V)
+{
+	int rows = M.size();
+	int cols = M[0].size();
+
+	// Step 1: Matrix-Vector Multiplication
+	vector<float> result(rows, 0.0f);
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
+		{
+			result[i] += M[i][j] * V[j];
+		}
+	}
+
+	// Step 2: Check if the result is a scalar multiple of the original vector
+	float ratio = 0.0f;
+	bool initialized = false;
+	for (int i = 0; i < V.size(); ++i)
+	{
+		if (V[i] != 0)
+		{
+			float currentRatio = result[i] / V[i];
+			if (!initialized)
+			{
+				ratio = currentRatio; // Set the initial ratio
+				initialized = true;
+			}
+			else if (fabs(currentRatio - ratio) > 1e-5)
+			{
+				// If ratios are not equal, return a null vector (not an eigenvector)
+				return vector<float>(V.size(), 0.0f);
+			}
+		}
+	}
+
+	// If all components are scalar multiples, return the original vector (it is an eigenvector)
+	return V;
 }
 
 /**
@@ -82,32 +141,32 @@ vector<vector<float>> R_Matrix_calc(matrix* M) {
  * @return A vector containing the eigenvalues of the input matrix, rounded to a specified number of decimal places.
  */
 
-vector<float> eigen_values(matrix* M) {
-	
+vector<float> eigen_values(matrix *M)
+{
+
 	auto mat = M;
 
-	for (int _ = 0; _ < 3; _++) {
+	for (int _ = 0; _ < 3; _++)
+	{
 
-		matrix* R = new matrix(R_Matrix_calc(mat));
-		matrix* Q = new matrix(orthogonalization(mat));
-		auto prod = matrix_product(R,Q);
+		matrix *R = new matrix(R_Matrix_calc(mat));
+		matrix *Q = new matrix(orthogonalization(mat));
+		auto prod = matrix_product(R, Q);
 		mat = new matrix(prod);
 		delete R;
 		delete Q;
 	}
-	
+
 	auto Mat = rounder(mat->get_matrix());
 
 	vector<float> eigen_values;
 
 	const int decimal_place = 2;
 	auto decimal_value = pow(10, decimal_place);
-	for (int i = 0; i < Mat.size(); i++) {
-		eigen_values.push_back(round(Mat[i][i] * decimal_value)/decimal_value);
+	for (int i = 0; i < Mat.size(); i++)
+	{
+		eigen_values.push_back(round(Mat[i][i] * decimal_value) / decimal_value);
 	}
-	
-	return(eigen_values);
 
+	return (eigen_values);
 }
-
-
